be89e066da3d85ef182c64d838d3383dcc7965a9
Derek Graetz <dgraetz@gmail.com>
2018-02-04 18:09:44 -0200

    Chapter 5 finished

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,11 +19,26 @@
 
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            {{-- {{  $thread->replies }} --}}
             @foreach($thread->replies as $reply)
                 @include ('threads.reply')
             @endforeach
         </div>
     </div>
+    
+    @if(auth()->check())
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <form method="POST" action="{{ $thread->path().'/replies' }}">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <textarea name="body" id="body" class="form-control" placeholder="Have Something to Say?" rows="5"></textarea>
+                </div>
+                <button type="submit" class="btn btn-default">Post</button>
+            </form>
+        </div>
+    </div>
+    @else
+    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to perticipate in this discussion.</p>
+    @endif
 </div>
 @endsection
